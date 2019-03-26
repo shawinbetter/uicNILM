0a87c90136ff2bb5b7ec0126dc2fb454fcda6eb3
Alienware18 <1254548379@qq.com>
2019-03-26 23:58:36 +0800

    Init visualization_index

--- uicNILM/dashboard/views.py
+++ uicNILM/dashboard/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     context = dict()
     return render(request, 'dashboard-index.html', context)
+
+
+def visualization_index(request):
+    context = dict()
+    return render(request, 'dashboard-index.html', context)
